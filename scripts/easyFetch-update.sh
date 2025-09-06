b65d61d122d512e87be249c1f179e29881be267f
ChilleFielmann <mail@florianprinz.de>
2025-09-06 21:19:31 +0200

    Update easyFetch-update.sh

--- scripts/easyFetch-update.sh
+++ scripts/easyFetch-update.sh
@@ -1,1 +1,14 @@
+#! /bin/bash
 
+#Repository herunterladen
+wget https://github.com/ChilleFielmann/easyFetch/archive/refs/heads/main.tar.gz -O /home/aixconcept/easyFetch/repo.tar.gz &&
+
+#Nur scripts und preps entpacken
+tar -xzf repo.tar.gz easyFetch-main/scripts/ /home/aixconcept/easyFetch/scripts &&
+tar -xzf repo.tar.gz easyFetch-main/preps/ /home/aixconcept/easyFetch/preps &&
+
+#Archiv löschen
+rm -f /home/aixconcept/easyFetch/repo.tar.gz &&
+
+#Finish
+echo "Nice! easyFetch ist nun auf dem neusten Stand!"
