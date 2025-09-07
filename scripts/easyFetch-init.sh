230e234fd1170283250e4d6095a91087d9a407a5
ChilleFielmann <mail@florianprinz.de>
2025-09-07 12:16:56 +0200

    Update easyFetch-init.sh

--- scripts/easyFetch-init.sh
+++ scripts/easyFetch-init.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#exec > /dev/null
 
 #Variablen
 version="1.0"
 kunde=""
-standort="" 
+standort=""
 
 #Kunde abfragen
 echo -n "Kunde:"
@@ -14,17 +13,17 @@ read standort
 
 #Packete upgraden
 echo "[INFO]Pakete werden aktualisiert..."
-apt update > /dev/null
-apt -y upgrade > /dev/null
+apt update
+apt -y upgrade
 echo "[OK]Pakete aktualisiert!" &&
 
 #Pakete installieren
 echo "[INFO]Pakete werden installiert..."
-apt install -y tldr > /dev/null
-apt install -y ipcalc > /dev/null
-apt install -y nmap > /dev/null
-apt install -y openvpn > /dev/null
-snap install yq > /dev/null
+apt install -y tldr 
+apt install -y ipcalc 
+apt install -y nmap 
+apt install -y openvpn 
+snap install yq 
 echo "[OK]" &&
 
 #Maschine umbenennen
@@ -42,14 +41,14 @@ echo "[OK]" &&
 #Aliase Anlegen
 echo "[INFO]Aliase werden angelegt..."
 sleep 1
-runuser -l aixconcept -c "touch /home/aixconcept/.bash_aliases" &&
-runuser -l aixconcept -c "echo 'alias easyFetch-update="/home/aixconcept/easyFetch/scripts/easyFetch-update.sh"' >> /home/aixconcept/.bash_aliases" &&
+runuser -l aixconcept -c "touch /home/aixconcept/.bash_aliases"  &&
+runuser -l aixconcept -c "echo 'alias easyFetch-update="/home/aixconcept/easyFetch/scripts/easyFetch-update.sh"' >> /home/aixconcept/.bash_aliases"  &&
 source /home/aixconcept/.bash_aliases
 echo "[OK]" &&
 
 #Repository Herunterladen
 echo "[INFO]Repository wird heruntergeladen..."
-wget https://github.com/ChilleFielmann/easyFetch/archive/refs/heads/main.tar.gz -O /home/aixconcept/repo.tar.gz &&
+wget https://github.com/ChilleFielmann/easyFetch/archive/refs/heads/main.tar.gz -O /home/aixconcept/repo.tar.gz  &&
 echo "[OK]" &&
 echo "[INFO]Repository wird entpackt..."
 tar -xzf /home/aixconcept/repo.tar.gz -C /home/aixconcept &&
